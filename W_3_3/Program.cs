8d454527dc53ee9e70583b26a7fa546c59efe4f6
Olesya <H_lesya00@mail.ru>
2022-07-19 23:05:52 +0300

    W_3_3

---
+++ W_3_3/Program.cs
@@ -0,0 +1,20 @@
+﻿/* 
+Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N. 
+ 
+3 -> 1, 8, 27 
+5 -> 1, 8, 27, 64, 125 
+*/ 
+ 
+Console.WriteLine("Enter Number(N) "); 
+int RundomeNumber = int.Parse(Console.ReadLine()); 
+ 
+int Rnum = RundomeNumber; 
+    int digitR = 1; 
+    while (Rnum >= digitR) 
+    { 
+        int result = digitR*digitR*digitR; 
+        digitR++; 
+ 
+ 
+        Console.WriteLine( "Tabl_cubov: " + result); 
+    }
