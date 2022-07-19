2ae891240d01af91a7041523b633292dd7011182
Olesya <H_lesya00@mail.ru>
2022-07-19 23:04:39 +0300

    W_3_1

---
+++ W_3_1/Program.cs
@@ -0,0 +1,31 @@
+﻿/* 
+Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом. 
+ 
+14212 -> нет 
+ 
+12821 -> да 
+ 
+23432 -> да 
+*/ 
+ 
+Console.WriteLine("Enter number: "); 
+int number = int.Parse(Console.ReadLine()); 
+ 
+Palindrom(number); 
+ 
+void Palindrom (int num) 
+{ 
+   int FormulaFirst = number % 10; 
+   int FormulaSecond = number / 10000; 
+    if (FormulaFirst == FormulaSecond) 
+   { 
+      FormulaFirst = (FormulaFirst * 10) / 100; 
+      FormulaSecond = (FormulaSecond * 10) % 10; 
+ 
+   } 
+   if (FormulaFirst == FormulaSecond)  
+      Console.WriteLine("Yes, polindrom " + num);  
+ 
+      else 
+      Console.WriteLine("No, polindrom " + num); 
+}
